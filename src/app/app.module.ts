827c05f96ce85e9fe27b38890c8da7be2fc4e225
an <an832n@att.com>
2018-08-08 23:54:58 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent,
+    OrderListComponent,
+    ItemListComponent,
+    ConfigurationComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
